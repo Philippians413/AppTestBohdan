fe4a6c3de973b7c56dc25ca0d75fc3003010a36c
Andrii Rodionov <Andrii.Rodionov@gmail.com>
2015-09-20 13:29:49 +0300

    modify app

--- src/main/java/ua/yandex/rd/App.java
+++ src/main/java/ua/yandex/rd/App.java
@@ -1,10 +1,7 @@
 package ua.yandex.rd;
 
 public class App {
-  public static double avg(double ... arr) {
-    double sum = 0.0;
-    for (double val : arr)
-      sum += val;
-    return sum / arr.length;
+  public static double findTempClosestToZero(double ... arr) {
+    return 0;
   }
 }
